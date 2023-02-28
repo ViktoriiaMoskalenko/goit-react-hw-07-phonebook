9930c7da0b3bacd6ea3c1e6801a37632daa8c9be
Viktoriia Moskalenko <vika01mos@gmail.com>
2023-02-28 11:53:37 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from '../redux/operations';
+
+import { ContactList } from './Phonebook/ContactList'
+import { ContactForm } from './Phonebook/ContactForm'
+import { Filter } from './Phonebook/Filter'
+import styles from './Phonebook/Phonebook.module.css'
+
+export function App() {
+  const isLoading = useSelector(state => state.contacts.isLoading);
+  const error = useSelector(state => state.contacts.error);
+    const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+    return (
+      <div>
+        <h1 className={styles.title}>Phonebook</h1>
+        <ContactForm/>
+        <h2 className={styles.title}>Contacts</h2>
+        <Filter />
+        {isLoading && !error && <b>Request in progress...</b>}
+        <ContactList/>
+      </div>
+    )
+  }
